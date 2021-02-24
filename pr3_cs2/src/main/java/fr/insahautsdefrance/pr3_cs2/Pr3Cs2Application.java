7ba16f9ab963b113e769077fd229ae0eaf619b2e
Quentin Derpion <quentin@derpion.fr>
2021-02-24 09:53:05 +0100

    [PR3_CS2] Initialisaation du projet

---
+++ pr3_cs2/src/main/java/fr/insahautsdefrance/pr3_cs2/Pr3Cs2Application.java
@@ -0,0 +1,13 @@
+package fr.insahautsdefrance.pr3_cs2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr3Cs2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pr3Cs2Application.class, args);
+	}
+
+}
---
+++ pr3_cs2/src/test/java/fr/insahautsdefrance/pr3_cs2/Pr3Cs2ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insahautsdefrance.pr3_cs2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pr3Cs2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
