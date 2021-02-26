e4246fd96fa84632ee52cd9b4d9c23b97a6d505e
nicocau <niicolascauchois@gmail.com>
2021-02-26 10:21:22 +0100

    [CS1] ajoute le kafka + la convertion json to obj

---
+++ cs1/src/main/java/fr/insahautsdefrance/cs1/Model/CovidModel.java
@@ -0,0 +1,11 @@
+package fr.insahautsdefrance.cs1.Model;
+
+import java.util.ArrayList;
+
+public class CovidModel {
+    private String ID;
+    private String Message;
+    private Global Global;
+    private ArrayList<Countries> Countries;
+    private String Date;
+}
